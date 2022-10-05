b6f5de3efa729bb107554723cdcc752e38523813
Rodrigo <rodrigo@crowdbotics.com>
2022-10-05 20:35:11 +0000

    Add Blank Screen 3

---
+++ screens/BlankScreen3/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen3 = () => {
+  return <View style={_styles.BlankScreen3}>
+      </View>;
+};
+
+export default BlankScreen3;
+
+const _styles = StyleSheet.create({
+  BlankScreen3: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});

000f2b5375159d35b8a46fb3ee6d46564527d7d9
Rodrigo <rodrigo@crowdbotics.com>
2022-10-05 20:46:10 +0000

    Add Blank Screen 4

---
+++ screens/BlankScreen4/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen4 = () => {
+  return <View style={_styles.BlankScreen4}>
+      </View>;
+};
+
+export default BlankScreen4;
+
+const _styles = StyleSheet.create({
+  BlankScreen4: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});

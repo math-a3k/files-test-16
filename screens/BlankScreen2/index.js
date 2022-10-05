4d6709cbf36d66e497e1df5f1ee80d47e8eeb4b5
Rodrigo <rodrigo@crowdbotics.com>
2022-10-05 20:21:51 +0000

    Add Blank Screen 2

---
+++ screens/BlankScreen2/index.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const BlankScreen2 = () => {
+  return <View style={_styles.BlankScreen2}>
+      </View>;
+};
+
+export default BlankScreen2;
+
+const _styles = StyleSheet.create({
+  BlankScreen2: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
